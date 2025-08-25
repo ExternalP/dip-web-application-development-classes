2df11f59b502766118cadd61b4c5599300914baf
ExternalP <110000600+ExternalP@users.noreply.github.com>
2025-08-25 14:31:42 +0800

    session 6

--- session-06/intro_to_state/js_starter/scripts_challenge_1.js
+++ session-06/intro_to_state/js_starter/scripts_challenge_1.js
@@ -1,16 +1,27 @@
 // Challenge - Stage 1
+let addBook = function (book) {
+    // If there's no book to do, do nothing
+    // data
+    if (!book || book.length < 1) return;
 
-// If there's no book to do, do nothing
-// data
+    // Get the list
+    // let list = document.querySelector('#app > #list');
+    let list = document.querySelector('#app');
 
-// Get the list
+    // Create a new list item
+    // let listItem = document.createElement('li');
+    let listItem = document.createElement('p');
 
-// Create a new list item
-// template
+    listItem.textContent = book;
 
-// Append the item to the list
-// render
+    // Append the item to the list
+    list.appendChild(listItem);
+};
 
 let book = "Harry Potter IV";
 
 addBook(book);
+
+
+
+
